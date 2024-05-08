f3127f8a62cc2ddd3d09ffe8f4a59a27752b4012
DESKTOP-UPQIIMM\kota1 <kota1888-haru@outlook.jp>
2024-05-08 17:19:12 +0900

    ローディングシーンの練習中

--- RhythmGame/Assets/Mikku/scripts/SceneToMain.cs
+++ RhythmGame/Assets/Mikku/scripts/SceneToMain.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     public void change_button()
     {
-        SceneManager.LoadScene("MainScene");
+        LoadingScreenManager.LoadScene("MainScene");
     }
-
 }
